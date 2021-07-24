68fd9d429dc9e11d2efe5ba7ba68b7665c9ca979
DShulkin <thedavidshulkin@gmail.com>
2021-07-24 13:25:24 -0400

    99% complete

--- js/app.js
+++ js/app.js
@@ -1,1 +1,64 @@
-baguetteBox.run('.gallery');​in
+window.addEventListener('load', function() {
+  baguetteBox.run('.gallery');
+});
+
+
+const captions = [
+  '',
+
+ 'I love hay bales. Took this snap on a drive through the countryside past some straw fields.',
+
+ 'The lake was so calm today. We had a great view of the snow on the mountains from here.',
+
+ 'Canyon I hiked to the top of the mountain and got this picture of the canyon and trees below.',
+
+ 'It was amazing to see an iceberg up close, it was so cold but didn’t snow today.',
+
+ 'The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons.',
+
+ 'Fall is coming, I love when the leaves on the trees start to change color.',
+
+ 'I drove past this plantation yesterday, everything is so green!',
+
+ 'My summer vacation to the Oregon Coast. I love the sandy dunes!',
+
+ 'We enjoyed a quiet stroll down this countryside lane.',
+
+ 'Sunset at the coast! The sky turned a lovely shade of orange.',
+
+ 'I did a tour of a cave today and the view of the landscape below was breathtaking.',
+
+ 'I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in.'
+];
+
+
+const getImages = (array) => {
+    let images = ''; 
+        for (let i = 1; i < array.length; i++) {
+            images += `<a href="images/${i}.jpg" class="image" data-caption="${array[i]}"> 
+                          <img src="images/thumbnails/${i}.jpg">
+                       </a>`;
+        }
+        return images; 
+}  
+document.querySelector('.gallery').innerHTML = `${getImages(captions)}`;
+
+
+
+let searchBar = document.querySelector('.search');
+let image = document.querySelectorAll('.image');
+
+const searchFilter = () => {
+    let userInput = searchBar.value.toLowerCase();
+
+    for (let i = 0; i < image.length; i++){
+      let dataCaption = image[i].getAttribute('data-caption').toLowerCase();
+
+      if (dataCaption.includes(userInput)) {
+        image[i].style.display = 'block';
+      } else {
+        image[i].style.display = 'none';
+      }
+    }
+  };
+ searchBar.addEventListener('keyup', searchFilter); 
